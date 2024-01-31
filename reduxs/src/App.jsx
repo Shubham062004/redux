47d8a709c58a9e4ae84f84376f5f5a5854847acd
Shubham062004 <chaurasiaamrita400@gmail.com>
2024-02-01 03:36:55 +0530

    update

---
+++ reduxs/src/App.jsx
@@ -0,0 +1,12 @@
+import Button from './components/LikeCounter';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Button/>
+    </div>
+  );
+}
+
+export default App;
